ef26ab1c9e183fcba0129601d0f838b2ea37ff11
jmbl2-svg <jmbl2@cin.ufpe.br>
2026-06-08 11:11:23 -0300

    Insiht 1 corrigido

--- analysis/analise_desigualdade_regional.sql
+++ analysis/analise_desigualdade_regional.sql
@@ -1,16 +1,16 @@
 -- Análise da Distribuição Geográfica e Desigualdade Regional de Bolsas
 WITH fato AS (
-    SELECT * FROM {{ ref('fato_prouni_dbt') }}
+    SELECT * FROM public.fato_prouni_dbt
 ),
 dim_localidade AS (
-    SELECT * FROM {{ ref('dim_localidade_dbt') }}
+    SELECT * FROM public.dim_localidade_dbt
 )
 
 SELECT 
     l.regiao_beneficiario_bolsa AS regiao,
     l.sigla_uf_beneficiario_bolsa AS estado,
     COUNT(*) AS total_bolsas,
-    ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER(), 2) AS percentual_nacional
+    ROUND(COUNT() * 100.0 / SUM(COUNT()) OVER(), 2) AS percentual_nacional
 FROM fato f
 INNER JOIN dim_localidade l 
     ON f.id_localidade_sk = l.id_localidade_sk
