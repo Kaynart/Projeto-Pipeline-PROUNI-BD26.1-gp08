54bec709a252ff1363adffd7d4c98be79afb7f78
jmbl2-svg <jmbl2@cin.ufpe.br>
2026-06-08 11:25:08 -0300

    Todos os Insights Corrigidos

--- analysis/analise_perfil_social.sql
+++ analysis/analise_perfil_social.sql
@@ -1,16 +1,16 @@
 -- Análise do Perfil Social dos Beneficiários (Gênero e Raça)
 WITH fato AS (    
-    SELECT * FROM {{ ref('fato_prouni_dbt') }}
+    SELECT * FROM public.fato_prouni_dbt
 ),
 dim_beneficiario AS (
-    SELECT * FROM {{ ref('dim_beneficiario_dbt') }}
+    SELECT * FROM public.dim_beneficiario_dbt
 )
 
 SELECT 
     b.sexo_beneficiario_bolsa AS genero,
     b.raca_beneficiario_bolsa AS raca,
     COUNT(*) AS total_bolsas,
-    ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER(), 2) AS percentual_do_total
+    ROUND(COUNT() * 100.0 / SUM(COUNT()) OVER(), 2) AS percentual_do_total
 FROM fato f
 INNER JOIN dim_beneficiario b 
     ON f.id_beneficiario_sk = b.id_beneficiario_sk
